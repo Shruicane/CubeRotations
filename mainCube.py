3f41ea5f55c143cc90907635f91f76d0fd9c0ed1
Bagpigs <73357341+Bagpigs@users.noreply.github.com>
2020-10-23 19:25:38 +0200

    first cube

---
+++ mainCube.py
@@ -0,0 +1,39 @@
+import Cube
+import random
+
+# region functions
+
+def scramble(cube):
+    moves = random.randint(10,20)
+    for i in range(moves):
+        cube = cube.rotate(random.randint(0,11))
+    return cube
+
+def rekFunk(turn,cube,funcWay):
+    if turn == turns:
+        if cube.check(stage):
+            global stepCube
+            for i in funcWay:
+                way.append(i)
+            stepCube = cube
+            return True
+        return False
+    else:
+        for i in range(18):
+            funcWay.append(i)
+            newCube = cube.rotate(i)
+            if rekFunk(turn+1, newCube, funcWay):
+                return True
+        return False
+
+# endregion
+
+defaultCube = Cube()
+way = []
+stepCube = scrumble(defaultCube)
+
+for stage in range(10):
+    turns = 0
+    print ("Running Stage %.", stage)
+    while not rekFunk(0,stepCube,[]):
+        turns+=1
