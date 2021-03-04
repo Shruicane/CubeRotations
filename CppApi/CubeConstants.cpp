2a164e21babbdd8e53b5fa88991db484127fb6dc
Konrad <k.brandts@arcor.de>
2021-03-04 22:47:39 +0100

    add support for constants

---
+++ CppApi/CubeConstants.cpp
@@ -0,0 +1,70 @@
+#include "CubeConstants.h"
+
+// colors
+const int CubeConstants::COLOR_EMPTY;
+const int CubeConstants::COLOR_WHITE;
+const int CubeConstants::COLOR_RED;
+const int CubeConstants::COLOR_BLUE;
+const int CubeConstants::COLOR_ORANGE;
+const int CubeConstants::COLOR_GREEN;
+const int CubeConstants::COLOR_YELLOW;
+
+// Faces
+const int CubeConstants::FACE_WHITE;
+const int CubeConstants::FACE_RED;
+const int CubeConstants::FACE_BLUE;
+const int CubeConstants::FACE_ORANGE;
+const int CubeConstants::FACE_GREEN;
+const int CubeConstants::FACE_YELLOW;
+
+
+// Checks
+const int CubeConstants::CHECK_CROSS;
+
+const int CubeConstants::CHECK_CORNER_1;
+const int CubeConstants::CHECK_CORNER_2;
+const int CubeConstants::CHECK_CORNER_3;
+const int CubeConstants::CHECK_CORNER_4;
+
+const int CubeConstants::CHECK_EDGE_1;
+const int CubeConstants::CHECK_EDGE_2;
+const int CubeConstants::CHECK_EDGE_3;
+const int CubeConstants::CHECK_EDGE_4;
+
+const int CubeConstants::CHECK_YELLOW_CROSS_POSITIONS;
+const int CubeConstants::CHECK_YELLOW_CROSS_FACES;
+
+const int CubeConstants::CHECK_YELLOW_CORNER_POSITIONS;
+const int CubeConstants::CHECK_YELLOW_CORNER_FACE_1;
+const int CubeConstants::CHECK_YELLOW_CORNER_FACE_2;
+const int CubeConstants::CHECK_YELLOW_CORNER_FACE_3;
+const int CubeConstants::CHECK_YELLOW_CORNER_FACE_4;
+
+
+// face checks
+const int WHITE_FACE_SOLVED = 0;
+const int RED_FACE_SOLVED = 0;
+const int BLUE_FACE_SOLVED = 0;
+const int ORANGE_FACE_SOLVED = 0;
+const int GREEN_FACE_SOLVED = 0;
+const int YELLOW_FACE_SOLVED = 0;
+
+// rotates
+const int CubeConstants::ROTATE_LEFT;
+const int CubeConstants::ROTATE_LEFT_PRIME;
+const int CubeConstants::ROTATE_RIGHT;
+const int CubeConstants::ROTATE_RIGHT_PRIME;
+const int CubeConstants::ROTATE_UP;
+const int CubeConstants::ROTATE_UP_PRIME;
+const int CubeConstants::ROTATE_DOWN;
+const int CubeConstants::ROTATE_DOWN_PRIME;
+const int CubeConstants::ROTATE_FRONT;
+const int CubeConstants::ROTATE_FRONT_PRIME;
+const int CubeConstants::ROTATE_BACK;
+const int CubeConstants::ROTATE_BACK_PRIME;
+const int CubeConstants::ROTATE_X_SLICE;
+const int CubeConstants::ROTATE_X_SLICE_PRIME;
+const int CubeConstants::ROTATE_Y_SLICE;
+const int CubeConstants::ROTATE_Y_SLICE_PRIME;
+const int CubeConstants::ROTATE_Z_SLICE;
+const int CubeConstants::ROTATE_Z_SLICE_PRIME;
